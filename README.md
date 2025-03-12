ae84e068cb47220d977c38f762e5de90333c152b
kirito-hn <luis-n.p@live.com>
2025-03-12 12:57:13 -0300

    projeto finalizado 1.0

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Projeto proposto pela trilha de aprendizagem do ONE NEXT EDUCATION, parceria da 
 Projeto visa a pratica da Logica de programação, sua proposta é a criação da logica por traz de um site que recebe alguns nomes do usuaria os salva e sorteia um nome entre eles. 
 
 Liguagens utilizadas: Java Script, HTML e CSS.
+
+
+   <p>
+        A página é constituida por um campo onde usuário irá digitar um nome, ao clicar em adicionar, o nome digitado será inserido em um lista e exibido na página.
+
+        Ao clicar no botão sortear amigo, o sistema irá sorear um indice da lista de amigos e irá exebir o nome sorteado para o usuário.
+
+
+    </p>
--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function verificarNome(){
+    let nome=document.querySelector('input').value.trim();
+    if (nome!=""){
+        if(nomes.includes(nome)){
+            alert("Nome já existe no array, digite um nome diferente");
+        }else{
+            nomes.push(nome);
+        }
+    }else{
+        alert("Por favor, insira um nome.");
+    } 
+    limparCampo();
+    apagarLista('listaAmigos');
+    atualizarLista();
+}
+
+function sortearAmigo(){
+    let qtdAmigos=nomes.length;
+    let sorteio=Math.floor((Math.random()*qtdAmigos));
+    let resultado = document.getElementById("resultado");
+    apagarLista('listaAmigos');
+    resultado.innerHTML = "Amigo sorteado é " + nomes[sorteio];
+}
+
+function atualizarLista(nome){
+    let lista = document.getElementById("listaAmigos");
+    apagarLista('listaAmigos');
+    for(i=0;i<=nomes.length;i++){
+        let item = document.createElement('li');
+        item.textContent=nomes[i];
+        lista.appendChild(item);
+    }
+}
+
+function apagarLista(elemento){
+    let lista=document.getElementById(elemento);
+    lista.innerHTML="";
+
+}
+
+function limparCampo(){
+    lista=document.querySelector('input');
+   lista.value='';
+}
+
+
+function reiniciar(){
+    nomes=[];
+    apagarLista('listaAmigos');
+    apagarLista('resultado');
+
+}
